949162947cdbd97b1a6e4096cbd9083c55a1e9c7
Anthony Castrio <anthonycastrio@Anthonys-MacBook-Air.local>
2016-11-21 15:18:10 -0500

    init commit

---
+++ app/src/main/java/edu/umd/hcil/impressionistpainter434/ImpressionistView.java
@@ -0,0 +1,235 @@
+package edu.umd.hcil.impressionistpainter434;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by jon on 3/20/2016.
+ */
+public class ImpressionistView extends View {
+
+    private ImageView _imageView;
+
+    private Canvas _offScreenCanvas = null;
+    private Bitmap _offScreenBitmap = null;
+    private Paint _paint = new Paint();
+
+    private int _alpha = 150;
+    private int _defaultRadius = 25;
+    private Point _lastPoint = null;
+    private long _lastPointTime = -1;
+    private boolean _useMotionSpeedForBrushStrokeSize = true;
+    private Paint _paintBorder = new Paint();
+    private BrushType _brushType = BrushType.Square;
+    private float _minBrushRadius = 5;
+
+    private ArrayList<DrawRect> _drawRectList = new ArrayList<DrawRect>();
+
+    private class DrawRect {
+        Rect _rect = new Rect();
+        Paint _paint = new Paint();
+
+        DrawRect(Rect rect, Paint paint) {
+            _rect = rect;
+            _paint = paint;
+        }
+
+        void drawDrawRect (Canvas canvas) {
+            canvas.drawRect(_rect, _paint);
+        }
+    }
+
+    public ImpressionistView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ImpressionistView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ImpressionistView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    /**
+     * Because we have more than one constructor (i.e., overloaded constructors), we use
+     * a separate initialization method
+     * @param attrs
+     * @param defStyle
+     */
+    private void init(AttributeSet attrs, int defStyle){
+
+        // Set setDrawingCacheEnabled to true to support generating a bitmap copy of the view (for saving)
+        // See: http://developer.android.com/reference/android/view/View.html#setDrawingCacheEnabled(boolean)
+        //      http://developer.android.com/reference/android/view/View.html#getDrawingCache()
+        this.setDrawingCacheEnabled(true);
+
+        _paint.setColor(Color.RED);
+        _paint.setAlpha(_alpha);
+        _paint.setAntiAlias(true);
+        _paint.setStyle(Paint.Style.FILL);
+        _paint.setStrokeWidth(4);
+
+        _paintBorder.setColor(Color.BLACK);
+        _paintBorder.setStrokeWidth(3);
+        _paintBorder.setStyle(Paint.Style.STROKE);
+        _paintBorder.setAlpha(50);
+
+        //_paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.MULTIPLY));
+    }
+
+    @Override
+    protected void onSizeChanged (int w, int h, int oldw, int oldh){
+
+        Bitmap bitmap = getDrawingCache();
+        Log.v("onSizeChanged", MessageFormat.format("bitmap={0}, w={1}, h={2}, oldw={3}, oldh={4}", bitmap, w, h, oldw, oldh));
+        if(bitmap != null) {
+            _offScreenBitmap = getDrawingCache().copy(Bitmap.Config.ARGB_8888, true);
+            _offScreenCanvas = new Canvas(_offScreenBitmap);
+        }
+    }
+
+    /**
+     * Sets the ImageView, which hosts the image that we will paint in this view
+     * @param imageView
+     */
+    public void setImageView(ImageView imageView){
+        _imageView = imageView;
+    }
+
+    /**
+     * Sets the brush type. Feel free to make your own and completely change my BrushType enum
+     * @param brushType
+     */
+    public void setBrushType(BrushType brushType){
+        _brushType = brushType;
+    }
+
+    /**
+     * Clears the painting
+     */
+    public void clearPainting(){
+        //TODO
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if(_offScreenBitmap != null) {
+            canvas.drawBitmap(_offScreenBitmap, 0, 0, _paint);
+        }
+
+        // Draw the border. Helpful to see the size of the bitmap in the ImageView
+        canvas.drawRect(getBitmapPositionInsideImageView(_imageView), _paintBorder);
+
+//        for (DrawRect drawRect : _drawRectList) {
+//            drawRect.drawDrawRect(_offScreenCanvas);
+//        }
+
+    }
+
+    /*
+     * Modified From:
+     *  - http://stackoverflow.com/questions/7807360/how-to-get-pixel-colour-in-android
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent){
+
+        //TODO
+        //Basically, the way this works is to listen for Touch Down and Touch Move events and determine where those
+        //touch locations correspond to the bitmap in the ImageView. You can then grab info about the bitmap--like the pixel color--
+        //at that location
+        Rect img_rect = new Rect(this.getBitmapPositionInsideImageView(_imageView));
+        int touchx = (int) motionEvent.getX();
+        int touchy = (int) motionEvent.getY();
+        Rect new_rect = new Rect(touchx, touchy, touchx + 20, touchy + 20);
+//        ImageView imageView = _imageView;
+//        Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+//        int pixel = bitmap.getPixel(new_rect.centerX(), new_rect.centerY());
+
+
+        Bitmap imageViewBitmap = _imageView.getDrawingCache();
+       // int pixel = imageViewBitmap.getPixel(img_rect.centerX(), img_rect.centerY());
+        int pixel = imageViewBitmap.getPixel(touchx, touchy);
+        _paint.setColor(pixel);
+        //int pixel = _imageView.getPixel(img_rect.centerX(), img_rect.centerY());
+        Paint drawPaint = new Paint(pixel);
+        DrawRect drawRect = new DrawRect(new_rect, drawPaint);
+        _drawRectList.add(drawRect);
+        invalidate();
+
+        return true;
+    }
+
+
+
+
+    /**
+     * This method is useful to determine the bitmap position within the Image View. It's not needed for anything else
+     * Modified from:
+     *  - http://stackoverflow.com/a/15538856
+     *  - http://stackoverflow.com/a/26930938
+     * @param imageView
+     * @return
+     */
+    private static Rect getBitmapPositionInsideImageView(ImageView imageView){
+        Rect rect = new Rect();
+
+        if (imageView == null || imageView.getDrawable() == null) {
+            return rect;
+        }
+
+        // Get image dimensions
+        // Get image matrix values and place them in an array
+        float[] f = new float[9];
+        imageView.getImageMatrix().getValues(f);
+
+        // Extract the scale values using the constants (if aspect ratio maintained, scaleX == scaleY)
+        final float scaleX = f[Matrix.MSCALE_X];
+        final float scaleY = f[Matrix.MSCALE_Y];
+
+        // Get the drawable (could also get the bitmap behind the drawable and getWidth/getHeight)
+        final Drawable d = imageView.getDrawable();
+        final int origW = d.getIntrinsicWidth();
+        final int origH = d.getIntrinsicHeight();
+
+        // Calculate the actual dimensions
+        final int widthActual = Math.round(origW * scaleX);
+        final int heightActual = Math.round(origH * scaleY);
+
+        // Get image position
+        // We assume that the image is centered into ImageView
+        int imgViewW = imageView.getWidth();
+        int imgViewH = imageView.getHeight();
+
+        int top = (int) (imgViewH - heightActual)/2;
+        int left = (int) (imgViewW - widthActual)/2;
+
+        rect.set(left, top, left + widthActual, top + heightActual);
+
+        return rect;
+    }
+}
+
